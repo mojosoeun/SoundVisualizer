19178525ef1544674d144f7351a8a609b50619d4
sona <mojosoeun@gmail.com>
2016-04-14 23:46:39 +0900

    Update visualizer.js

--- app/scripts/visualizer.js
+++ app/scripts/visualizer.js
@@ -2,10 +2,52 @@
     'use strict';
 
     function SoundVisualizerService() {
+        
         var service = {};
         var audioCtx = null;
         var _stream = null;
         var intv = null;
+        
+        function drawCanvas(dataArray,bufferLength){
+      
+          var canvas = document.getElementById('visualizer');
+          var canvasCtx = canvas.getContext("2d");
+    
+          var WIDTH = 300;
+          var HEIGHT = 300;
+    
+          canvasCtx.fillStyle = 'rgb(242, 242, 242)';
+          canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+    
+          canvasCtx.lineWidth = 2;
+          canvasCtx.beginPath();
+    
+          var sliceWidth = WIDTH * 1.0 / bufferLength;
+          var x = 0;
+    
+          for(var i = 0; i < bufferLength; i++) {
+            var data = dataArray[i];
+            var v = data / 128.0;
+            var y = v * HEIGHT/2;
+    
+            var r = data + 120 ;
+            var g = 255 -  data ;
+            var b = data / 3;
+    
+            canvasCtx.strokeStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    
+            if(i === 0) {
+              canvasCtx.moveTo(x, y);
+            } else {
+              canvasCtx.lineTo(x, y);
+            }
+    
+            x += sliceWidth;
+          }
+          canvasCtx.lineTo(canvas.width, canvas.height/2);
+          canvasCtx.stroke();
+        }
+        
         service.start = function(){
           navigator.getUserMedia  = navigator.getUserMedia ||
                                   navigator.webkitGetUserMedia ||
@@ -53,46 +95,9 @@
       return service;
     }
 
-    function drawCanvas(dataArray,bufferLength){
-      var canvas = document.getElementById('visualizer');
-      var canvasCtx = canvas.getContext("2d");
+    
 
-      var WIDTH = 300;
-      var HEIGHT = 300;
-
-      canvasCtx.fillStyle = 'rgb(242, 242, 242)';
-      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-      canvasCtx.lineWidth = 2;
-      canvasCtx.beginPath();
-
-      var sliceWidth = WIDTH * 1.0 / bufferLength;
-      var x = 0;
-
-      for(var i = 0; i < bufferLength; i++) {
-        var data = dataArray[i];
-        var v = data / 128.0;
-        var y = v * HEIGHT/2;
-
-        var r = data + 120 ;
-        var g = 255 -  data ;
-        var b = data / 3;
-
-        canvasCtx.strokeStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
-
-        if(i === 0) {
-          canvasCtx.moveTo(x, y);
-        } else {
-          canvasCtx.lineTo(x, y);
-        }
-
-        x += sliceWidth;
-      }
-      canvasCtx.lineTo(canvas.width, canvas.height/2);
-      canvasCtx.stroke();
-    }
-
-    angular.module('SmartMirror')
+    angular.module('soundVisualizerApp')
     .factory('SoundVisualizerService', SoundVisualizerService);
 
 }());
