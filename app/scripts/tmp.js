6b2c962f22d81feda321f76767dbc17bbc0baf81
sona <mojosoeun@gmail.com>
2016-06-08 22:28:54 +0900

    change stroke color
    change stroke color

--- app/scripts/tmp.js
+++ app/scripts/tmp.js
@@ -76,10 +76,8 @@
     var WIDTH = 150;
     var HEIGHT = 150;
 
-    canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+    canvasCtx.fillStyle = 'rgb(242, 242, 242)';
     canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-    canvasCtx.strokeStyle = 'rgb(255,255,255)';
-
     canvasCtx.lineWidth = 2;
     canvasCtx.beginPath();
 
@@ -90,6 +88,12 @@
       var data = dataArray[i];
       var v = data / 128.0;
       var y = v * HEIGHT/2;
+
+      var r = data + 120 ;
+      var g = 255 -  data ;
+      var b = data / 3;
+
+      canvasCtx.strokeStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
 
       if(i === 0) {
         canvasCtx.moveTo(x, y);
