450dda76075e1d09bec781b027bdcdc0e6559245
soeun <mojosoeun@gmail.com>
2016-07-21 00:12:55 +0900

    feat : add rectangle

---
+++ app/scripts/sketch.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  var canvas = document.getElementById('visualizer');
+  if (!canvas) {
+    console.log('Failed to retrieve the <canvas> element');
+    return;
+  }
+
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'rgba(0,0,255,1.0)';
+  ctx.fillRect(120,10,150,150);
+
+}());
