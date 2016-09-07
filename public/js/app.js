09bfc17db28c8cd2fb5f8a3d737272e433aeb170
soeun <mojosoeun@gmail.com>
2016-09-08 00:25:07 +0900

    feat: add particle code

--- public/js/app.js
+++ public/js/app.js
@@ -71,6 +71,82 @@ function drawBar() {
   // canvasCtx.fillStyle = 'rgb(255, 255, 255)';
   canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
+  // var posX = 20,
+  //     posY = canvas.height / 2;
+  //
+  // var particles = {},
+  //     particleIndex = 0,
+  //     settings = {
+  //       density: 20,
+  //       particleSize: 10,
+  //       startingX: canvas.width / 2,
+  //       startingY: canvas.height / 4,
+  //       gravity: 0.5
+  //     };
+  //
+  // // Set up a function to create multiple particles
+  // function Particle() {
+  //   // Establish starting positions and velocities
+  //   this.x = settings.startingX;
+  //   this.y = settings.startingY;
+  //
+  //   // Determine original X-axis speed based on setting limitation
+  //   this.vx = Math.random() * 20 - 10;
+  //   this.vy = Math.random() * 20 - 5;
+  //
+  //   // Add new particle to the index
+  //   // Object used as it's simpler to manage that an array
+  //   particleIndex ++;
+  //   particles[particleIndex] = this;
+  //   this.id = particleIndex;
+  //   this.life = 0;
+  //   this.maxLife = 100;
+  // }
+  //
+  // // Some prototype methods for the particle's "draw" function
+  // Particle.prototype.draw = function() {
+  //   this.x += this.vx;
+  //   this.y += this.vy;
+  //
+  //   // Adjust for gravity
+  //   this.vy += settings.gravity;
+  //
+  //   // Age the particle
+  //   this.life++;
+  //
+  //   // If Particle is old, it goes in the chamber for renewal
+  //   if (this.life >= this.maxLife) {
+  //     delete particles[this.id];
+  //   }
+  //
+  //   // Create the shapes
+  //   canvasCtx.clearRect(settings.leftWall, settings.groundLevel, canvas.width, canvas.height);
+  //   canvasCtx.beginPath();
+  //   canvasCtx.fillStyle="#ffffff";
+  //   // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
+  //   canvasCtx.arc(this.x, this.y, settings.particleSize, 0, Math.PI*2, true);
+  //   canvasCtx.closePath();
+  //   canvasCtx.fill();
+  //
+  // }
+  //
+  // canvasCtx.fillStyle = "rgba(10,10,10,0.8)";
+  // canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+  //
+  // // Draw the particles
+  // for (var i = 0; i < settings.density; i++) {
+  //   if (Math.random() > 0.97) {
+  //     // Introducing a random chance of creating a particle
+  //     // corresponding to an chance of 1 per second,
+  //     // per "density" value
+  //     new Particle();
+  //   }
+  // }
+  //
+  // for (var i in particles) {
+  //   particles[i].draw();
+  // }
+
   var barWidth = (WIDTH / audiosource.bufferLength) * 2.5;
   var barHeight;
   var barData;
@@ -156,6 +232,13 @@ var canvas = document.getElementById('viewport');
 var canvasCtx = canvas.getContext('2d');
 var two = new Two({fullscreen: true}).appendTo(elem);
 var position = new Two.Vector(two.width/2, two.height/2);
+var posX = 20,
+    posY = canvas.height / 2;
+// Initial velocities
+var vx = 10,
+    vy = -10,
+    gravity = 1;
+
 
 window.addEventListener('resize', resizeCanvas, false);
 function resizeCanvas() {
