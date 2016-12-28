e5260ef7cfdef8a07ac9940df6c723a255adb2cf
mojosoeun <mojosoeun@gmail.com>
2016-12-29 00:10:54 +0900

    feat: add shell script for deploy

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglifyjs');
 var minify = require('gulp-minify');
 var concat = require('gulp-concat');
-var jshint = require('gulp-jshint');
 
 gulp.task('scripts', function() {
   return gulp.src(['./src/js/config.js', './src/js/modules/*.js', './src/js/index.js'])
@@ -19,8 +18,8 @@ gulp.task('css', function() {
     .pipe(gulp.dest('./public/'));
 });
 
-gulp.task('lint', function() {
-  return gulp.src('./src/js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default', { verbose: true }));
+gulp.task('watch', function() {
+  gulp.watch('./src/js/*.js', ['scripts']);
 });
+
+gulp.task('default', ['css', 'scripts']);
