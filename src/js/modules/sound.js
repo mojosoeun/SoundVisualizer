725cc02437ab6c7be4e64c64fb1994ec9cc29430
sona <mojosoeun@gmail.com>
2017-07-10 14:20:38 +0900

    fix: resolve autoplay issue in chrome mobile

--- src/js/modules/sound.js
+++ src/js/modules/sound.js
@@ -48,8 +48,9 @@ var sound = (function(soundcloud) {
     },
     'play': function(streamUrl){
       this.audio.src = 'data:audio/mpeg;base64,/+MYxAAAAANIAUAAAASEEB/jwOFM/0MM/90b/+RhST//w4NFwOjf///PZu////9lns5GFDv//l9GlUIEEIAAAgIg8Ir/JGq3/+MYxDsLIj5QMYcoAP0dv9HIjUcH//yYSg+CIbkGP//8w0bLVjUP///3Z0x5QCAv/yLjwtGKTEFNRTMuOTeqqqqqqqqqqqqq/+MYxEkNmdJkUYc4AKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq';
+      this.audio.play();
       this.audio.setAttribute('src', streamUrl);
-      this.audio.play()
+      this.audio.play();
     }
   };
 
