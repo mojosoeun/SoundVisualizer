2bc882e5e641ca60e3816df23c7fc9a9e4ce51dd
Jin Park <jin+github@jinpark.net>
2017-08-19 02:25:24 +0900

    skip ci

--- src/js/modules/sound.js
+++ src/js/modules/sound.js
@@ -3,7 +3,7 @@ var sound = (function(soundcloud) {
   'use strict';
 
   var client_id = "802c2f1c80c96881ff265799929e8a2c";
-  console.log('soud');
+
   function sound(audio) {
     if ( !(this instanceof sound) ) {
       soundcloud.initialize({
